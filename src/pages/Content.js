8359164bae86a8594182e15495fcaa0da3e2e20d
Acesif <asifpalak@gmail.com>
2023-05-21 00:28:37 +0600

    tab system and separate listing

--- src/pages/Content.js
+++ src/pages/Content.js
@@ -3,14 +3,24 @@ import { context } from '../App';
 const Content = () => {
   const [task,setTask] = useState("");
   const [tab,setTab] = useContext(context)
+  const projectList = JSON.parse(localStorage.getItem("Sections"))
+  const projectTitle = projectList[tab].value;
+  const [itemList, setItemList] = useState([])
+  const addToList = (task) => {
+    setItemList([...itemList,task])
+  }
+  useEffect(()=>{
+    localStorage.setItem(`${tab}`,JSON.stringify(itemList))
+  },[itemList,tab])
   return (
     <div className='content'>
         <h2>Checklist</h2>
+        <h3>Tab: {projectTitle}</h3>
         <div>
           <div className="content-container mb-3">
             <label className="form-label">
               <input type="text" onChange={(e)=>setTask(e.target.value)} value={task} className="form-control" />
-              <button onClick={()=> setTask("")}>Add</button>
+              <button onClick={()=> addToList(task)}>Add</button>
               {
                 useEffect(()=>{
                   localStorage.setItem(tab,task)
@@ -18,8 +28,18 @@ const Content = () => {
               }
             </label>
           </div>
-          </div>
         </div>
+        {
+          itemList.map((i)=>(
+            <div className="form-check item-list">
+              <label className="form-check-label">
+                <input className="form-check-input" type="checkbox" />
+                {i}
+              </label>
+            </div>
+          ))
+        }
+    </div>
   )
 }
 
